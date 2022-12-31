491ef6946038853b65cd1696ba1b6af5c9075752
Anasse LAARABI <contact.laarabi@gmail.com>
2022-12-31 11:35:44 +0100

    Update package && App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import { useEffect, useState } from 'react';
 import MyNavbar from './components/navBar/MyNavbar';
+import MobileNav from './components/navBar/MobileNav';
+
+function useWindowWidth() {
+  const [windowSize, setWindowSize] = useState(undefined);
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    handleResize();
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  return windowSize;
+}
 
 function App() {
+  const size = useWindowWidth();
   return (
     <div className="App">
       <header className="App-header">
-        <MyNavbar />
+        {size < 768 ? <MobileNav /> : <MyNavbar />}
       </header>
     </div>
   );

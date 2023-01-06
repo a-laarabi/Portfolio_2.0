e645e0221a72021f99b919edbaf3f2d30cd002b2
Anasse LAARABI <contact.laarabi@gmail.com>
2023-01-06 11:04:00 +0100

    Create Projects.js

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,28 @@
+import { Container } from 'react-bootstrap';
+import projectsObj from '../../assets/projectsObj';
+import Project from './Project';
+import './project.css';
+
+function Projects() {
+  return (
+    <Container>
+      <div className="projects">
+        {
+          projectsObj.map((project) => (
+            <Project
+              key={project.id}
+              title={project.title}
+              img={project.img}
+              description={project.description}
+              projectDetailSecond={project.projectDetailSecond}
+              githubLink={project.githubLink}
+              deployLink={project.deployLink}
+            />
+          ))
+        }
+      </div>
+    </Container>
+  );
+}
+
+export default Projects;

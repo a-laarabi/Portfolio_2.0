bc92cbb631c73ca1c5d9b68ca66619a8a1a57cbc
Anasse LAARABI <contact.laarabi@gmail.com>
2023-01-09 21:40:07 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,28 +1,30 @@
 import { Container } from 'react-bootstrap';
 import projectsObj from '../../assets/projectsObj';
-import Project from './Project_A';
+import Project from './Project_B';
 import './project.css';
 
 function Projects() {
   return (
-    <Container>
-      <div className="projects">
-        {
-          projectsObj.map((project) => (
-            <Project
-              key={project.id}
-              id={project.id}
-              title={project.title}
-              img={project.img}
-              description={project.description}
-              projectDetailSecond={project.projectDetailSecond}
-              githubLink={project.githubLink}
-              deployLink={project.deployLink}
-            />
-          ))
-        }
-      </div>
-    </Container>
+    <div className="projects_section">
+      <Container>
+        <div className="projects">
+          {
+            projectsObj.map((project) => (
+              <Project
+                key={project.id}
+                id={project.id}
+                title={project.title}
+                img={project.img}
+                description={project.description}
+                projectDetailSecond={project.projectDetailSecond}
+                githubLink={project.githubLink}
+                deployLink={project.deployLink}
+              />
+            ))
+          }
+        </div>
+      </Container>
+    </div>
   );
 }
 

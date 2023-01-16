00d5d88c092f698a5213b4b81f41e5f42601532d
Anasse LAARABI <contact.laarabi@gmail.com>
2023-01-16 20:52:22 +0100

    Update Contact.js

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Container, Row, Col } from 'react-bootstrap';
+import { BsLinkedin } from 'react-icons/bs';
 import './contact.css';
 
 function Contact() {
@@ -15,19 +19,34 @@ function Contact() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(formData);
-    // code to send formData to the server
+
+    emailjs.send(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, formData, process.env.REACT_APP_PUBLIC_KEY)
+      .then(() => {
+        toast.success('🎉 Message sent successfully.. Thank You');
+        setFormData({
+          name: '',
+          email: '',
+          message: '',
+        });
+      }, () => {
+        toast.error('FAILED...');
+      });
   };
 
   return (
     <div className="contact" id="contact">
+      <ToastContainer />
       <Container>
         <div className="contactIntro">
           <h2>Let&apos;s Connect</h2>
           <p>Want to connect with me or collaborate on projects together, You can send me a message and I&apos;ll reply as soon as I can.. Thank You.</p>
-          <a className="linkedinBtn" href="https://www.linkedin.com/in/a-laarabi/">LinkedIn</a>
+          <a className="linkedinBtn" href="https://www.linkedin.com/in/a-laarabi/">
+            LinkedIn
+            {' '}
+            <BsLinkedin />
+          </a>
         </div>
-        <form action="https://formspree.io/f/xnqwwrdy" method="post">
+        <form onSubmit={handleSubmit}>
           <Row>
             <Col xs={12} md={6}>
               <label htmlFor="name">
